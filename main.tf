429a7c34a7cf0742e06c0223909071dd32f968e5
amn-bmore <78953332+amn-bmore@users.noreply.github.com>
2021-05-24 13:23:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,43 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.13.3"
+
 }
 
-provider "random" {}
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
 
-resource "random_pet" "table_name" {}
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
+module "vpc" {
+  source = "app.terraform.io/amitnayak-training/vpc/aws"
 
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-  attribute {
-    name = "UUID"
-    type = "S"
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
+
+resource "aws_eip" "nat" {
+  count = 3
+
+  vpc = true
+}
+
